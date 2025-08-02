e00b14f6f3b4f8b65e6530a1b74e15d33fcc556a
meyor <meyorpop@gmail.com>
2025-08-02 16:42:07 +0100

    uodate the footer

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -378,64 +378,6 @@
 </section>
 
 <!-- Footer -->
-{{-- <section class="footer3 cid-uNkyFLRjqw" once="footers" id="footer-3-uNkyFLRjqw" style="padding-top: 6rem; padding-bottom: 6rem; background-color: #000000;">
-  <div class="container">
-    <div class="row">
-      <div class="row-links">
-        <ul class="header-menu">
-          <li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-            <a href="/about" class="text-white animate__animated animate__delay-1s animate__fadeIn">About</a>
-          </li>
-          <li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-            <a href="#" class="text-white animate__animated animate__delay-1s animate__fadeIn">Terms</a>
-          </li>
-          <li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-            <a href="#" class="text-white animate__animated animate__delay-1s animate__fadeIn">Privacy</a>
-          </li>
-          <li class="header-menu-item mbr-fonts-style display-5 animate__animated animate__delay-1s animate__fadeIn">
-            <a href="#" class="text-white animate__animated animate__delay-1s animate__fadeIn">Contact</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-12 mt-4">
-        <div class="social-row">
-          <div class="soc-item">
-            <a href="#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-              <span class="mbr-iconfont socicon socicon-facebook display-7"></span>
-            </a>
-          </div>
-          <div class="soc-item">
-            <a href="#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-              <span class="mbr-iconfont socicon-twitter socicon"></span>
-            </a>
-          </div>
-          <div class="soc-item">
-            <a href="#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-              <span class="mbr-iconfont socicon-instagram socicon"></span>
-            </a>
-          </div>
-          <div class="soc-item">
-            <a href="#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-              <span class="mbr-iconfont socicon socicon-linkedin"></span>
-            </a>
-          </div>
-          <div class="soc-item">
-            <a href="#" target="_blank" class="animate__animated animate__delay-1s animate__fadeIn">
-              <span class="mbr-iconfont socicon socicon-twitch"></span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 mt-5">
-        <p class="mbr-fonts-style copyright display-7 animate__animated animate__delay-1s animate__fadeIn">
-          © 2025 Plasu University
-        </p>
-      </div>
-    </div>
-  </div>
-</section> --}}
 @include('components.footer')
 @include('components.scripts')
 
